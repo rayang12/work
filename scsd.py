0959afbb95c299a4c960f3e14af743988c220462
rayang12 <44171535+rayang12@users.noreply.github.com>
2018-10-16 18:59:36 +0530

    Initial commit

---
+++ scsd.py
@@ -0,0 +1,15 @@
+# Add two numbers
+a = 3
+b = 4
+sum = a + b
+print(sum)
+
+subtract = a - b
+print(subtract)
+
+print ("sanskari programmer") 
+
+c = (1 + 2 + 3 +
+     4 + 5 + 6 +
+     7)
+     
